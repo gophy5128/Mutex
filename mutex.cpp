0374a8c79325ba28218ba137bf7666d102dc9868
pingchungchang <83915045+pingchungchang@users.noreply.github.com>
2021-08-15 13:45:35 +0800

    added_costumer

--- mutex.cpp
+++ mutex.cpp
@@ -12,42 +12,69 @@
 typedef struct Data{
     int id;
     int seconds;
+    int status;
     int data;
+    RWZONE* buffer;
 }Data;
+typedef struct RWZONE{
+    int arr[1000];
+    int wnext;
+    int rnext;
+    int status;
+}RWZONE;
 
-Data Buffer[1000];
-
-void ProducerFunction(void* arg)
-{
+void ProducerFunction(void* arg){
     Data* write = (Data*)arg;
     int s = (rand()%10+1)*100000;
-    for (int i = 0; i < 5; i++){
+    while(true){
         usleep(s);
+        while(write->buffer->status != write->id){
+            if(write->buffer->status == 0&&write->buffer->wnext-write->buffer->rnext<1000)write->buffer->status = write->id;
+        }
         write->seconds = time((time_t*)NULL);
         write->data = (rand() %10);
-        printf("Producer %d write data: time is %d, data is %d", *write->id, *write->seconds. *write->data);
+        printf("Producer %d write data: time is %d, data is %d", write->id, write->seconds. write->data);//write->id,not *write->id
+        write->buffer->arr[write->buffer->wnext]= write->data;
+        write->buffer->wnext++;
     }
+}
 
 
-void CustomerFunction(void* arg)
-{
-
+void CustomerFunction(void* arg){
+    Data* reader = (Data*)arg;
+    int s = (rand()%10+1)*100000;
+    while(true){
+        usleep(s);
+        while(reader->buffer->status != -reader->id){
+            if(reader->buffer->status == 0&&reader->buffer->rnext<=reader->buffer->wnext)reader->buffer->status = -reader->id;
+        }
+        while(reader->buffer->rnext == reader->buffer->wnext);
+        printf("Customer %d reads data: time is %d, data is %d\n",reader->id,s,reader->buffer->arr[reader->buffer->rnext]);
+        reader->buffer->rnext ++;
+        reader->buffer->status = 0;
+    }
 }
 
 int main()
 {
+    RWZONE* buffer;
+    buffer->wnext = 0;
+    buffer->rnext = 0;
+    buffer->status = -99;
     for(int i=0; i<2; i++)
     {
-        Data* producer;
+        Data* producer = new Data;
         producer->id = i+1;
+        producer->buffer = buffer;
         pthread_t t;
         pthread_create(&t, NULL, ProduceFunction, (void*) producer);
         pthread_join(t, NULL);
     }
     for(int i=0; i<3; i++)
     {
-        Data* customer;
+        Data* customer = new Data;
         customer->id = i+1;
+        customer->buffer = buffer;
         pthread_t t;
         pthread_create(&t, NULL, CustomerFunction, (void*) customer);
         pthread_join(t, NULL);
